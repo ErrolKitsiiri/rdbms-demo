0662e373291782d16aa5ed0b0ca48643f4d437ec
ErrolKitsiiri <errolantonio325@gmail.com>
2026-01-15 16:40:51 +0300

    initialization

---
+++ src/main/java/com/example/rbdms_demo/RbdmsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rbdms_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbdmsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbdmsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rbdms_demo/RbdmsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rbdms_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbdmsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
